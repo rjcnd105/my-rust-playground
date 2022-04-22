b169b86266f494d22a003f2e43af9c5023d4c856
hjm1 <rjcnd123@gmail.com>
2022-04-23 01:55:01 +0900

    .

--- searchstring/src/main.rs
+++ searchstring/src/main.rs
@@ -6,7 +6,6 @@ use std::process;
 use searchstring::Config;
 
 fn main() {
-
     // std::env::args 함수는 반복자(iterator) 형식으로 커맨드라인 인자들을 우리 프로그램에 전달해줍니다.
     // 반복자에 collect 함수 호출을 통해 반복자가 생성하는 일련의 값을 벡터로 변환할 수 있습니다.s
     let args: Vec<String> = env::args().collect();
@@ -32,5 +31,3 @@ fn main() {
         process::exit(1);
     }
 }
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
+use std::assert_matches::debug_assert_matches;
+use std::intrinsics::discriminant_value;
+
 pub mod myFile;
